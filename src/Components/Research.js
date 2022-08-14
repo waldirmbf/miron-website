781eabebeceab42a4f327363b76dcd9b8389192a
lrodrigues <lrodrigues@powercosts.com>
2022-08-14 18:38:19 -0500

    Removing unused var

--- src/Components/Research.js
+++ src/Components/Research.js
@@ -3,17 +3,6 @@ import React, { Component } from 'react';
 class Research extends Component {
 	render() {
 
-		if (this.props.data) {
-			var research = this.props.data.researches.map(function (research) {
-				var projectImage = 'images/research/' + research.image;
-				return <div key={research.title}>
-					<img alt={research.title} src={projectImage} />
-					<h2>{research.title}</h2>
-					<p>{research.description}</p>
-				</div>
-			})
-		}
-
 		return (
 			<section id="research">
 
