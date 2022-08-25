a07586d84666d6e1388f2f08a84837223beac1ff
waldirmbf <Biologia21!>
2022-08-25 11:04:17 -0500

    Adding analytics

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
 			resumeData: {}
 		};
 
-		ReactGA.initialize('UA-110570651-1');
+		ReactGA.initialize('UA-116120309-1');
 		ReactGA.pageview(window.location.pathname);
 
 	}
