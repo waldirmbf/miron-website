d23455ea47b8598499c99aba80c677a36747eed2
lrodrigues <lrodrigues@pcienergysolutions.com>
2023-06-11 18:22:29 -0500

    Modifying date

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,81 +1,68 @@
 import React, { Component } from 'react';
 import { Icon } from '@iconify/react';
-
-
-
+import preval from 'preval.macro';
 
 class Footer extends Component {
 
-	
 
-	render() {
 
-		// getCurrentDate(){
+    render() {
 
-		// 	let newDate = new Date()
-		// 	let date = new Date().getDate();
-		// 	let month = new Date().getMonth() + 1;
-		// 	let year = new Date().getFullYear();
-			
-		// 	return `${new Date().getFullYear()}${"/"}${new Date().getMonth() + 1<10?`0${new Date().getMonth() + 1}`:`${new Date().getMonth() + 1}`}${"/"}${new Date().getDate()}`
-		// }
-		
-		return (
-			<footer>
+        return (
+            <footer>
 
-				<div className="row">
-					<div className="twelve columns">
-						<ul className="social-links">
-							<li key="Google Scholar">
-								<a href="https://scholar.google.com/citations?user=30a5mKUAAAAJ&hl=en&oi=sra" target="_blank">
-									<Icon icon="academicons:google-scholar-square" />
-								</a>
-							</li>
-							<li key="ResearchGate" >
-								<a href="https://www.researchgate.net/profile/Waldir-Berbel-Filho" target="_blank">
-									<Icon icon="simple-icons:researchgate" />
-								</a>
-							</li>
-							<li key="Publons">
-								<a href="https://publons.com/wos-op/researcher/1417914/waldir-m-berbel-filho" target="_blank">
-									<Icon icon="academicons:publons-square" />
-								</a>
-							</li>
-							<li key="GitHub">
-								<a href="http://github.com/waldirmbf" target="_blank">
-									<Icon icon="akar-icons:github-fill" />
-								</a>
-							</li>
-							<li key="OrcID">
-								<a href="https://orcid.org/0000-0001-6991-4685" target="_blank">
-									<Icon icon="academicons:orcid" />
-								</a>
-							</li>
-							<li key="Twitter">
-								<a href="http://twitter.com/BerbelFilho" target="_blank">
-									<Icon icon="akar-icons:twitter-fill" />
-								</a>
-							</li>
-							<li key="Email">
-								<a href="mailto:waldirmbf@gmail.com" target="_blank">
-									<Icon icon="carbon:email" />
-								</a>
-							</li>
-						</ul>
+                <div className="row">
+                    <div className="twelve columns">
+                        <ul className="social-links">
+                            <li key="Google Scholar">
+                                <a href="https://scholar.google.com/citations?user=30a5mKUAAAAJ&hl=en&oi=sra" target="_blank">
+                                    <Icon icon="academicons:google-scholar-square" />
+                                </a>
+                            </li>
+                            <li key="ResearchGate" >
+                                <a href="https://www.researchgate.net/profile/Waldir-Berbel-Filho" target="_blank">
+                                    <Icon icon="simple-icons:researchgate" />
+                                </a>
+                            </li>
+                            <li key="Publons">
+                                <a href="https://publons.com/wos-op/researcher/1417914/waldir-m-berbel-filho" target="_blank">
+                                    <Icon icon="academicons:publons-square" />
+                                </a>
+                            </li>
+                            <li key="GitHub">
+                                <a href="http://github.com/waldirmbf" target="_blank">
+                                    <Icon icon="akar-icons:github-fill" />
+                                </a>
+                            </li>
+                            <li key="OrcID">
+                                <a href="https://orcid.org/0000-0001-6991-4685" target="_blank">
+                                    <Icon icon="academicons:orcid" />
+                                </a>
+                            </li>
+                            <li key="Twitter">
+                                <a href="http://twitter.com/BerbelFilho" target="_blank">
+                                    <Icon icon="akar-icons:twitter-fill" />
+                                </a>
+                            </li>
+                            <li key="Email">
+                                <a href="mailto:waldirmbf@gmail.com" target="_blank">
+                                    <Icon icon="carbon:email" />
+                                </a>
+                            </li>
+                        </ul>
 
-						<ul className="copyright">
-							<li>&copy; Copyright 2022</li>
-							<li>Waldir M. Berbel-Filho</li>
-							<li>Last Updated: {new Date().getMonth() + 1<10?`0${new Date().getMonth() + 1}`:`${new Date().getMonth() + 1}`}{"/"}{new Date().getDate()}{"/"}{new Date().getFullYear()}
-</li>
-						</ul>
+                        <ul className="copyright">
+                            <li>&copy; Copyright 2023</li>
+                            <li>Waldir M. Berbel-Filho</li>
+                            <li>Last Updated: {preval`module.exports = new Date().toLocaleString();`}</li>
+                        </ul>
 
-					</div>
-					<div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
-				</div>
-			</footer>
-		);
-	}
+                    </div>
+                    <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
+                </div>
+            </footer >
+        );
+    }
 }
 
 export default Footer;
