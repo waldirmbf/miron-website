6f86a06e34c6c4e1d9b089e6771ac450d1968fe7
Waldir M. Berbel-Filho <waldirmbf@gmail.com>
2023-07-01 11:36:04 -0500

    updating news and stuff

--- src/Components/News.js
+++ src/Components/News.js
@@ -9,9 +9,53 @@ class News extends Component {
 					<div className="row">
 						<h1>News</h1>
 						<div>
+							<p className="date"> January 23rd, 2023</p>
+							<p className="title"> Sperm-mediated paternal efffects project granted by OCAST! </p>
+							<p className="content"> Good news!!! We will be funded by <a href="https://oklahoma.gov/ocast/about-ocast.html" target="_blank">OCAST</a> to study the sperm-mediated paternal effects in poeciliids! More on that soon.</p>
+							<hr />
+						</div>
+						<div>
+							<p className="date"> September 26th, 2022</p>
+							<p className="title"> Third episode of book club on "The evolution of Beauty" available on Youtube. </p>
+							<p className="content"> Our Youtube channel <a href="https://www.youtube.com/channel/UCcjP3uH7VFk4n_lIc4bgxbQ" target="_blank">Ler Com Ciência</a>, aimed for portuguese-speakers, has now released its third <a href="https://www.youtube.com/watch?v=n5Eq9hnm4o4" target="_blank"> video </a> discussing the book "The Evolution of Beauty" by Richard Prum. This time we discuss the concept of evolutionary decandence via mate choice as well as sexual conflict in waterfowl via genitalia coevolution.</p>
+							<p>
+							 In PT: Nosso canal no Youtube <a href="https://www.youtube.com/channel/UCcjP3uH7VFk4n_lIc4bgxbQ" target="_blank">Ler Com Ciência</a>, lançou ontem seu terceiro <a href="https://www.youtube.com/watch?v=n5Eq9hnm4o4" target="_blank"> vídeo </a> discutindo o livro "The Evolution of Beauty" de Richard Prum.
+							 Desta vez discutimos o conceito de decadência evolutiva por escolha de parceiros, assim como o conflito sexual em aves aquáticas atavés da co-evolução da genitália. </p>
+							<hr />
+						</div>
+						<div>
+							<p className="date"> September 23rd, 2022</p>
+							<p className="title"> Sperm-mediated paternal effects project submitted to OCAST. </p>
+							<p className="content"> We are still looking for funding for our project on sperm-mediated paternal effects. We now submitted a proposal to <a href="https://oklahoma.gov/ocast/about-ocast.html" target="_blank">OCAST</a>. Fingers crossed!</p> 
+							<hr />
+						</div>
+						<div>
+							<p className="date"> September 14th, 2022</p>
+							<p className="title"> Second episode of book club on "The evolution of Beauty" available on Youtube. </p>
+							<p className="content"> Our Youtube channel <a href="https://www.youtube.com/channel/UCcjP3uH7VFk4n_lIc4bgxbQ" target="_blank">Ler Com Ciência</a>, aimed for portuguese-speakers, has now released its second <a href="https://www.youtube.com/watch?v=-_MyfV2YDZQ" target="_blank"> video </a> discussing the book "The Evolution of Beauty" by Richard Prum.
+							 This is a project in collaboration with my colleague <a href="https://g-pacheco.github.io/" target="_blank">George Pacheco</a> aiming to discuss and popularize science books, particularly in Evoltuionary Biology, that are yet to be translated into Portuguese. This project aims to encourage both readership and translation of important books in the field for a Portuguese-speaking audience (particularly Brazilian students). </p>
+							 
+							 <p>
+							 In PT: Nosso canal no Youtube <a href="https://www.youtube.com/channel/UCcjP3uH7VFk4n_lIc4bgxbQ" target="_blank">Ler Com Ciência</a>, lançou ontem seu segundo <a href="https://www.youtube.com/watch?v=-_MyfV2YDZQ" target="_blank"> vídeo </a> discutindo o livro "The Evolution of Beauty" de Richard Prum.
+							 Esse é um projeto em colaboração com meu amigo <a href="https://g-pacheco.github.io/" target="_blank">George Pacheco</a> voltado para a discussão e poularização de livros, particularmente de Biologia Evolutiva, que ainda não foram traduzidos para Português (ao menos no Brasil). Este projeto visa incentivar tanto a leitura como a tradução de importantes livros no campo para uma audiência cujo idioma nativo é o Português (particularmente estudantes brasileiros). </p>
+							 <hr />
+						</div>
+						<div>
+							<p className="date"> September 9th, 2022</p>
+							<p className="title">DNA methylation patterns between and F1 hybrids now published! </p>
+							<p className="content"> Our work on the DNA methylation patterns of parental <i>Kryptolebias</i> and their F1 hybrids is now available <a href="https://doi.org/10.1080/15592294.2022.2123014" target="_blank">here</a>.</p> 
+							<hr />
+						</div>
+						<div>
+							<p className="date"> August 30th, 2022</p>
+							<p className="title"><i>Kryptolebias</i> phylogenomics and  introgression paper now published!</p>
+							<p className="content"><i>Kryptolebias</i> phylogenomics, introgression and mito-nuclear discordances is now published. Open-access for 50 days using this <a href="https://authors.elsevier.com/a/1fgIM3m3nN6Qc1" target="_blank">link</a>. As always, feel free to reach out to discuss this study.</p>
+							<hr />
+						</div>
+						{/*<div>
 							<p className="date"> August 21st, 2022</p>
 							<p className="title">Welcome to my new website!</p>
-							<p className="content"> I am excited to launch my new webiste! More improvements to come (working now on a photo gallery). My previous <a href="https://berbelfilho.wixsite.com/home" target="_blank">webiste</a> will be discontinued soon.</p>
+							<p className="content"> I am excited to launch my new webiste! More improvements to come (working now on a photo gallery). My previous <a href="https://berbelfilho.wixsite.com/home" target="_blank">website</a> will be discontinued soon.</p>
 							<hr />
 						</div>
 						<div>
@@ -20,12 +64,12 @@ class News extends Component {
 							<p className="content">Our work on <i>Kryptolebias</i> phylogenomics, introgression and mito-nuclear discordances has been accepted at <a href="https://www.sciencedirect.com/journal/molecular-phylogenetics-and-evolution" target="_blank"> Molecular Phylogenetics and Evolution!</a> Very excting times!</p> 
 							<hr />
 						</div>
-						<div>
+						 <div>
 							<p className="date">August 6th, 2022</p>
 							<p className="title">New paper accepted at Epigenetics!</p>
 							<p className="content">Our work on the DNA methylation patterns of parental <i>Kryptolebias</i> and their hybrid is now accepted at <a href="https://www.tandfonline.com/journals/kepi20" target="_blank">Epigenetics</a>. More soon!</p> 
 							<hr />
-						</div>
+						</div> */}
 					</div>
 				</div>
 			</section >
--- src/Components/Research.js
+++ src/Components/Research.js
@@ -22,19 +22,19 @@ class Research extends Component {
 
 								<p>Most of my work has been using the unique genus killifish genus <b><i>Kryptolebias</i></b> to understand the causes and consequences of
 									the evolution of self-fertilization (selfing) in animals. <i>Kryptolebias</i> <b>comprise the known only case of vertebrates capable of selfing</b>, 
-									providing an an exceptional opportunity to ask why, when, and how selfing evolved, as well its ecological, phenotypic, and implications.
+									providing an exceptional opportunity to ask why, when, and how selfing evolved, as well its ecological, phenotypic, and evolutionary implications.
 								</p>
 
-								<p>How and why asexuality emerges from sexual ancestors is still an intriguing question. All asexual vertebrates have a
+								<p>How and why asexuality emerges from sexual ancestors is still an intriguing question. Most asexual vertebrates have a
 									hybrid origin. My postdoctoral research investigates the links between <b> hybridization and asexuality</b> in the Amazon Molly (<i>P. formosa</i>)
-									system. We are currently using a combination of cell biology methods (to investigate deviations from meiosis), quantitative
-									genetic analysis (QTL) from whole-genome sequencing data, phenotypic and experimental data aiming to understand how asexuality emerged
+									system. We are currently using a combination of cell biology methods (e.g., to investigate deviations from meiosis), quantitative
+									genetic analysis (QTL) from whole-genome sequencing data, phenotypic, and experimental data aiming to understand how asexuality emerged
 									on this system.</p>
-								<p>Finally, I recently developed an interest in understading how variation in gamete cells (sperm in particular), can contribute to population/species
-									differentiation. Taking advantage of the many livebearer species
-									available at <a href="https://international-stock-center-for-livebearing-fishes.oucreate.com/blog/" target="_blank">International Stock Center for Livebearing Fishes</a>,
+								<p>Finally, I recently developed an interest in understanding how variation in gamete cells (sperm in particular), can contribute to population/species
+									divergence. Taking advantage of the many livebearer species
+									available at <a href="https://international-stock-center-for-livebearing-fishes.oucreate.com/blog/" target="_blank"> The International Stock Center for Livebearing Fishes</a>,
 									we are testing several hypotheses regarding the effects of different environmental stressors and reproductive parameters on sperm variation
-									across different poecillid species. </p>
+									across different poeciliid species. </p>
 
 
 							</div>
@@ -43,9 +43,9 @@ class Research extends Component {
 							<div key="Phylogenetics, population genetics, and hybridization">
 								<img alt="Phylogenetics, population genetics, and hybridization" src="images/research/Cichla.jpg" />
 								<h2>Phylogenetics, population genetics, and hybridization</h2>
-								<p>Another area of my research revolves under the overlap between phylogenetics (the study of the relationships among species), population genetics (the genetic basis of the differentiation between populations), and the influence of hybridization and introgression have on species relationships and evolution.</p>
+								<p>Another area of my research revolves around the overlap between phylogenetics (the study of the relationships among species), population genetics (the genetic basis of the differentiation between populations), and the influence of hybridization and introgression have on species relationships and evolution.</p>
 
-								<p>Again,  <i> Kryptolebias</i> provides a fertile ground for this field. My research has revealed extensive hybridization in the genus, sometimes masking the true relationships in the genus. Remarkably, many of the introgression events involved the highly-selfing species <i> K. hermaphroditus</i>. I am currently studying how potential anthropogenic factors (e.g., pollution) may have influenced the extent of those hybridizations across natural populations in Brazil. </p>
+								<p>Again,  <i> Kryptolebias</i> provides a fertile ground for this field. My research has revealed extensive hybridization in the genus. Remarkably, many of the introgression events involved the highly-selfing species <i> K. hermaphroditus</i>. I am currently studying how potential anthropogenic factors (e.g., pollution) may have influenced the extent of those hybridizations across natural populations in Brazil, as well as testing how differences in mating systems can affect the likelihood of hybridization and introgression. </p>
 							</div>
 						</div>
 						<div className="research-content">
@@ -59,12 +59,12 @@ class Research extends Component {
 							</div>
 						</div>
 						<div className="research-content">
-							<div key="Development of epigenetics markers">
-								<img alt="Development of epigenetics markers" src="images/research/Anableps.jpeg" />
+							<div key="Development of epigenetics biomarkers">
+								<img alt="Development of epigenetics biomarkers" src="images/research/Anableps.jpeg" />
 								<h2>Development of epigenetics markers</h2>
-								<p>The identification of the degree autonomy between genomic and DNA methylation has the potential for identifying epigenetic markers which are exclusively environmentally-induced.
-									Epigenetic markers are applicable to a wide range of fields, and can be used to indirectly (and sometimes non-invasively) infer the environmental state of a particular sample based on the methylation status of its epigenetic marker.
-									Taking advantage of the clonal nature of both <i>Kryptolebias</i> and <i>P. formosa</i> and its multiple clonal lineages, I am interested to develop epigenetic (DNA methylation) markers, particularly in relation to pollution levels within the range found in natural populations.
+								<p>The identification of the degree autonomy between genomic and DNA methylation has the potential for identifying epigenetic biomarkers which are exclusively environmentally-induced.
+									Epigenetic biomarkers are applicable to a wide range of fields, and can be used to indirectly (and sometimes non-invasively) infer the environmental state of a particular sample based on the methylation status of its epigenetic biomarkers.
+									Taking advantage of the clonal nature of both <i>Kryptolebias</i> and <i>P. formosa</i> and its multiple clonal lineages, I am interested to develop epigenetic (DNA methylation) biomarkers, particularly in relation to pollution levels within the range found in natural populations.
 									This research holds the potential to provide practical molecular tools that can be used to assess different environmental stressors based on the methylation status of candidate genomic regions. </p>
 							</div>
 						</div>
